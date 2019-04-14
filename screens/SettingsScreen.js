321743b513a51c3e0fa48b0009203bc23d578a12
Angela <oliviablisskim@gmail.com>
2019-04-14 15:40:36 -0700

    remove default data

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import styles from "../styles/settingsScreen.styles";
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'Settings',
   };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View>
+      </View>
+    );
   }
 }
