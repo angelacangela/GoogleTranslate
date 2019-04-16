ff88a4d4db926e935ddfc7fbf17a0a204dc9bfa1
Angela <oliviablisskim@gmail.com>
2019-04-15 21:09:36 -0700

    add saved page styling

--- screens/savedScreen.js
+++ screens/savedScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { FlatList, View, ScrollView, StyleSheet, Text } from 'react-native';
+import { Icon } from "expo";
 import styles from "../styles/savedScreen.styles";
 
 export default class SavedScreen extends React.Component {
@@ -7,10 +8,36 @@ export default class SavedScreen extends React.Component {
     title: 'Saved',
   };
 
+  constructor(props) {
+    super(props);
+      this._renderEmptyListComponent = this._renderEmptyListComponent.bind(this)
+  }
+
+  _renderEmptyListComponent() {
+    return (
+      <View style={styles.savedEmptyContainer}>
+        <Icon.Ionicons
+          name={"md-star"}
+          size={90}
+          color={"lightgray"}
+          style={styles.savedStar}
+        />
+        <Text style={styles.savedEmptyText}>
+          Star a translation to see it here.
+        </Text>
+      </View>
+    )
+  }
+
   render() {
     return (
-      <ScrollView style={styles.container}>
-      </ScrollView>
+      <View style={styles.container}>
+        <FlatList
+          data={[]}
+          ListEmptyComponent={this._renderEmptyListComponent}
+          renderData={() => <View></View>}
+        />
+      </View>
     );
   }
 }
--- styles/savedScreen.styles.js
+++ styles/savedScreen.styles.js
@@ -1,9 +1,26 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
+const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 export default StyleSheet.create({
     container: {
+      alignItems: "center",
       flex: 1,
       paddingTop: 15,
       backgroundColor: '#fff',
-    }
+      justifyContent: "center"
+    },
+    savedEmptyContainer: {
+      flex: 1,
+      justifyContent: "center",
+      marginTop: SCREEN_HEIGHT * 0.3
+    },
+    savedEmptyText: {
+      color: "gray",
+      fontSize: 20,
+      fontWeight: "100",
+      textAlign: "center"
+    },
+    savedStar: {
+      textAlign: "center",
+    },
 });
